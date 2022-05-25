579fa6212d696a85757df0bce1f88438be87b07e
Pranathi <sripadapranathi@gmail.com>
2022-05-25 18:14:50 +0530

    change

--- Sliding Window/Longest Subarray with Ones after Replacement.cpp
+++ Sliding Window/Longest Subarray with Ones after Replacement.cpp
@@ -13,6 +13,13 @@ Example 2:
 Input: Array=[0, 1, 0, 0, 1, 1, 0, 1, 1, 0, 0, 1, 1], k=3
 Output: 9
 Explanation: Replace the '0' at index 6, 9, and 10 to have the longest contiguous subarray of 1s having length 9.
+
+Time Complexity #
+The time complexity of the above algorithm will be O(N) where ‘N’ is the count of numbers in the input array.
+
+Space Complexity #
+The algorithm runs in constant space O(1).
+
 */ 
 
 #include<bits/stdc++.h>
---
+++ Sliding Window/Permutation in a String.cpp
@@ -0,0 +1,37 @@
+/*
+Problem Statement #
+
+Given a string and a pattern, find out if the string contains any permutation of the pattern.
+
+Permutation is defined as the re-arranging of the characters of the string.
+For example, “abc” has the following six permutations:
+abc
+acb
+bac
+bca
+cab
+cba
+If a string has ‘n’ distinct characters it will have n!n! permutations.
+
+Example 1:
+
+Input: String="oidbcaf", Pattern="abc"
+Output: true
+Explanation: The string contains "bca" which is a permutation of the given pattern.
+Example 2:
+
+Input: String="odicf", Pattern="dc"
+Output: false
+Explanation: No permutation of the pattern is present in the given string as a substring.
+Example 3:
+
+Input: String="bcdxabcdy", Pattern="bcdyabcdx"
+Output: true
+Explanation: Both the string and the pattern are a permutation of each other.
+
+Time Complexity #
+The time complexity of the above algorithm will be O(N+M) where ‘N’ and ‘M’ are the number of characters in the input string and the pattern respectively.
+
+Space Complexity #
+The space complexity of the algorithm is O(M) since in the worst case, the whole pattern can have distinct characters which will go into the HashMap.
+*/
