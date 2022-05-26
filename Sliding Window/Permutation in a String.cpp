591f8f8b45a265364af0b7ba4a11cae230b0ca97
Pranathi <sripadapranathi@gmail.com>
2022-05-26 12:15:18 +0530

    Permutation

--- Sliding Window/Permutation in a String.cpp
+++ Sliding Window/Permutation in a String.cpp
@@ -18,11 +18,13 @@ Example 1:
 Input: String="oidbcaf", Pattern="abc"
 Output: true
 Explanation: The string contains "bca" which is a permutation of the given pattern.
+
 Example 2:
 
 Input: String="odicf", Pattern="dc"
 Output: false
 Explanation: No permutation of the pattern is present in the given string as a substring.
+
 Example 3:
 
 Input: String="bcdxabcdy", Pattern="bcdyabcdx"
@@ -35,3 +37,44 @@ The time complexity of the above algorithm will be O(N+M) where ‘N’ and ‘M
 Space Complexity #
 The space complexity of the algorithm is O(M) since in the worst case, the whole pattern can have distinct characters which will go into the HashMap.
 */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+bool findpermutation(string s,string p){
+    int start=0,matched=0;
+    unordered_map<char,int> freqmap;
+    for(auto i:p)
+        freqmap[i]++;
+    for(int i=0;i<s.length();i++){
+        if(freqmap.find(s[i])!=freqmap.end()){
+            freqmap[s[i]]--;   //decrement thr frequency of the matched character
+            if(freqmap[s[i]]==0)        //character is completely matched
+                matched++;
+        }
+
+        if(matched==freqmap.size())
+            return true;
+
+        if(i>=p.size()-1){  //shrink the window
+            if(freqmap.find(s[start])!=freqmap.end()){
+                if(freqmap[s[start]]==0)
+                    matched--;          //before putting the character back, decrement the matched count
+                freqmap[s[start]]++;
+            }
+            start++;
+        }
+    }
+    return false;
+}
+
+
+int main(){
+    string s,p;
+    cin>>s>>p;
+    if(findpermutation(s,p))
+        cout<<"true"<<endl;
+    else
+        cout<<"false"<<endl;
+
+}
