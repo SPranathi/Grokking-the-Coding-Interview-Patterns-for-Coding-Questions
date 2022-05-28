cdc1c25b856f3e5419dde074ea490bc19b52cd5e
Pranathi <sripadapranathi@gmail.com>
2022-05-28 15:14:40 +0530

    two pointers

--- Two Pointers/Pair with Target Sum.cpp
+++ Two Pointers/Pair with Target Sum.cpp
@@ -1,3 +1,4 @@
+/*
 Problem Statement #
 Given an array of sorted numbers and a target sum, find a pair in the array whose sum is equal to the given target.
 
@@ -14,3 +15,43 @@ Example 2:
 Input: [2, 5, 9, 11], target=11
 Output: [0, 2]
 Explanation: The numbers at index 0 and 2 add up to 11: 2+9=11 
+
+Time Complexity #
+The time complexity of the above algorithm will be O(N), where ‘N’ is the total number of elements in the given array.
+
+Space Complexity #
+The algorithm runs in constant space O(1).
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> targetpair(vector<int> v,int target){
+    int l=0,r=v.size()-1;
+    while(l<r){
+        if(target==v[l]+v[r])
+            return {l,r};
+        if(target>v[l]+v[r])
+            l++;
+        else
+            r--;
+    }
+    return {-1,-1};
+}
+
+int main(){
+    int x,target;
+    string s;
+    vector<int> v;
+    getline(cin,s);
+    stringstream ss(s);
+    cin>>target;
+    while(ss>>x){
+        v.push_back(x);
+        if(ss.peek()==' ')
+            ss.ignore();
+    }
+    vector<int> ans=targetpair(v,target);
+    for(auto i:ans)
+        cout<<i<<" ";
+}
