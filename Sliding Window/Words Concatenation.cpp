4bcfe17ab862c54a345c4a4b02fb9a3c8eac6702
Pranathi <sripadapranathi@gmail.com>
2022-05-28 09:32:52 +0530

    word concatenation

--- Sliding Window/Words Concatenation.cpp
+++ Sliding Window/Words Concatenation.cpp
@@ -22,3 +22,53 @@ Space Complexity #
 The space complexity of the algorithm is O(M) since at most, we will be storing all the words in the two HashMaps. In the worst case, we also need O(N) space for the resulting list.
 So, the overall space complexity of the algorithm will be O(M+N).
 */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> findwordConcatenation(string s,vector<string> words){
+    unordered_map<string,int> freqmap;
+    for(auto i:words)
+        freqmap[i]++;
+    
+    vector<int> res;
+    int wordcount=words.size(),wordlen=words[0].length();
+
+    for(int i=0;i<=s.length()-wordcount*wordlen;i++){
+        unordered_map<string,int> seen;
+        for(int j=0;j<wordcount;j++){
+            int nextindex=i+j*wordlen;
+            //get the next word from the string
+            string word=s.substr(nextindex,wordlen);
+            if(freqmap.find(word)==freqmap.end())   //break if we don't need this word
+                break;
+
+            seen[word]++;       //add the word to seen map
+
+            //no need to process further if the word has higher frequency than required
+            if(seen[word]>freqmap[word])
+                break;
+
+            if(j+1==wordcount)      //store index if we have found all the words
+                res.push_back(i);
+        }
+    }
+    return res;
+}
+
+int main(){
+    string s;
+    cin>>s;
+    vector<string> words;
+    string str,st;
+    getline(cin,str);
+    stringstream ss(str);
+    while(ss>>st){
+        words.push_back(st);
+        if(ss.peek()==' ')
+            ss.ignore();
+    }
+    vector<int> res=findwordConcatenation(s,words);
+    for(auto i:res)
+        cout<<i<<" ";
+}
