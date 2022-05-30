db7ecdff572dbb54e86c7b9ea7136ffeb55c908d
Pranathi <sripadapranathi@gmail.com>
2022-05-30 15:48:35 +0530

    triplets

--- Two Pointers/Triplet Sum to Zero.py
+++ Two Pointers/Triplet Sum to Zero.py
@@ -22,3 +22,35 @@ As we are calling searchPair() for every number in the input array, this means t
 Space complexity #
 Ignoring the space required for the output array, the space complexity of the above algorithm will be O(N) which is required for sorting.
 """
+
+def searchpair(arr,targetsum,l,triplets):
+    r=len(arr)-1
+    while(l<r):
+        cursum=arr[l]+arr[r]
+        if(cursum==targetsum):    #found the pair
+            triplets.append([-targetsum,arr[l],arr[r]])
+            l+=1
+            r-=1
+            while l<r and arr[l]==arr[l-1]:
+                l+=1    #skip same element to avoid duplicate triplets
+            while l<r and arr[r]==arr[r+1]:
+                r-=1    #skip same element to avoid duplicate triplets
+        elif targetsum>cursum:
+            l+=1    #we need a pair with a bigger sum
+        else:
+            r-=1    #we need a pair with a smaller sum
+
+def searchtriplets(arr):
+    arr.sort()
+    triplets=[]
+    for i in range(len(arr)):
+        if i>0 and arr[i]==arr[i-1]:     #skip same element to avoid duplicate triplets
+            continue
+        searchpair(arr,-arr[i],i+1,triplets)
+    return triplets
+
+def main():
+    li=list(map(int,input().split()))
+    print(searchtriplets(li))
+
+main()
