e880c696c719bfbcb02b7ee12d8967b9b6278578
Pranathi <sripadapranathi@gmail.com>
2022-05-30 17:08:59 +0530

    3sumsmaller

--- Two Pointers/Triplets with Smaller Sum.cpp
+++ Two Pointers/Triplets with Smaller Sum.cpp
@@ -24,3 +24,46 @@ Space complexity #
 Ignoring the space required for the output array, the space complexity of the above algorithm will be O(N) 
 which is required for sorting if we are not using an in-place sorting algorithm.
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+int searchpair(vector<int> &arr,int targetsum,int i){
+    int count=0,l=i+1,r=arr.size()-1;
+    while(l<r){
+        if(arr[l]+arr[r]<targetsum){
+            //since arr[r]>=arr[l], therefore, we can replace r by any number between
+            //l and r to get a sum less than the targetsum
+            count+=r-l;
+            l++;
+        }
+        else{
+            r--;    //we need a pair with a smaller sum
+        }
+    }
+    return count;
+}
+
+vector<vector<int>> searchtriplets(vector<int> &arr){
+    sort(arr.begin(),arr.end());
+    int count=0;
+    for(int i=0;i<arr.size()-2;i++){
+        count+=searchpair(arr,target-arr[i],i);
+    }
+    return count;
+}
+
+int main(){
+    vector<int> arr;
+    int x;
+    string s;
+    getline(cin,s);
+    stringstream ss(s);
+    while(ss>>x){
+        arr.push_back(x);
+        if(ss.peek()==' ')
+            ss.ignore();
+    }
+    int target;
+    cin>>target;
+    cout<<searchtriplet(arr,target)<<endl;
+}
