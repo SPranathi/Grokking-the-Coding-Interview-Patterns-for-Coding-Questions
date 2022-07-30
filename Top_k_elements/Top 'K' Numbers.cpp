ea18953f92b99ea574412d93dd38b5798da9b9f3
Pranathi <sripadapranathi@gmail.com>
2022-07-30 23:56:45 +0530

    k

---
+++ Top_k_elements/Top 'K' Numbers.cpp
@@ -0,0 +1,23 @@
+/*
+Problem Statement #
+
+Given an unsorted array of numbers, find the ‘K’ largest numbers in it.
+
+Example 1:
+
+Input: [3, 1, 5, 12, 2, 11], K = 3
+Output: [5, 12, 11] 
+
+Time complexity #
+
+As discussed above, the time complexity of this algorithm is O(K*logK+(N-K)*logK), which is asymptotically equal to O(N*logK)
+Space complexity #
+
+The space complexity will be O(K)O(K)O(K) since we need to store the top ‘K’ numbers in the heap.
+*/
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    
+}
